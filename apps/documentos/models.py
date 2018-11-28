f39efed08276ab2d09c3836eed50e8af075fb6ae
Diego Moraes <diegofrost96@gmail.com>
2018-11-27 21:32:18 -0300

    Criando os apps Documentos e Registro de Hora Extra

---
+++ apps/documentos/models.py
@@ -0,0 +1,9 @@
+# coding = utf-8
+
+from django.db import models
+
+class Documento(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
