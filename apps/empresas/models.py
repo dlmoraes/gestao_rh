a59baf52b38f1c6137fc0c2462135b99f090d9a4
dlmoraes <diegofrost96@gmail.com>
2018-12-02 11:15:47 -0300

    Cruds dos modelos

--- apps/empresas/models.py
+++ apps/empresas/models.py
@@ -1,9 +1,14 @@
 # coding = utf-8
 
 from django.db import models
+from django.urls import reverse_lazy
+
 
 class Empresa(models.Model):
     nome = models.CharField(max_length=100, help_text="Nome da empresa")
 
     def __str__(self):
         return self.nome
+
+    def get_absolute_url(self):
+        return reverse_lazy('home')
